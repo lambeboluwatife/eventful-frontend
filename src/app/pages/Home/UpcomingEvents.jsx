c4274c509431e9bf7f385a0a38e1279521afef4c
lambeboluwatife <boluwatifelambe@gmail.com>
2024-08-07 01:50:53 +0100

    fetch upcoming events from database

--- src/app/pages/Home/UpcomingEvents.jsx
+++ src/app/pages/Home/UpcomingEvents.jsx
@@ -1,20 +1,19 @@
 import EventList from "@/components/EventList";
-import { events } from "../../data/events";
 
 async function fetchEvents() {
   const response = await fetch(
     "https://altschool-eventful-backend.onrender.com/api/events"
   );
 
-  new Promise((resolve) => setTimeout(resolve, 2000));
+  // Simulate network delay for demo purposes
+  await new Promise((resolve) => setTimeout(resolve, 2000));
 
   const events = await response.json();
   return events.data;
 }
 
 const UpcomingEvents = async () => {
-  // const events = await fetchEvents()
-  // console.log(events)
+  const events = await fetchEvents();
   return (
     <div className="upcoming-events">
       <div className="container">
