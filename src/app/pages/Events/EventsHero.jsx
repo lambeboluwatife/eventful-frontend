5074c977028e9de59bd730eb14c61f70651bb5f9
lambeboluwatife <boluwatifelambe@gmail.com>
2024-08-11 09:32:54 +0100

    events page hero section

---
+++ src/app/pages/Events/EventsHero.jsx
@@ -0,0 +1,21 @@
+import Button from "@/components/Button";
+
+const EventsHero = () => {
+  return (
+    <section className="events-hero">
+      <div className="container">
+        <h3>Best events all over the</h3>
+        <h2>World</h2>
+        <h5>
+          Looking for things to do in around you? Whether you're a local, new in
+          town or just cruising through we've got loads of great tips and
+          events. You can explore by location, what's popular, our top picks,
+          free stuff... you got this. Ready?
+        </h5>
+        <Button text="Discover" />
+      </div>
+    </section>
+  );
+};
+
+export default EventsHero;
